cb18373038efdada26b9814de60572afef675d52
aprin418 <aprin418@gmail.com>
2021-10-10 23:29:18 -0400

    key

--- fetchRewardsPoints/settings.py
+++ fetchRewardsPoints/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+0=&m%)yux+r*087q4&jddnqf+hd-za=1^@-+9r3@a(7t_8)2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- frp/models.py
+++ frp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Payer(models.Model):
-    name = models.CharField(max_length=100)
-    points = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
     timestamp = models.DateField(auto_now_add=True)
 
     def __str__(self):
